25d70b5b816ced4d2aeff5fda85e071052e75d86
sureleo <surezeroleo@gmail.com>
2014-11-08 15:46:13 -0800

    http request lanuched

--- findcats/views.py
+++ findcats/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import urllib2
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+def index(req):
+    req = urllib2.Request('https://api.imgur.com/3/image/igmyoeU')
+    req.add_header('Authorization', 'Client-ID 195fbce43dd974e')
+    resp = urllib2.urlopen(req)
+    content = resp.read()
+    return HttpResponse(content)
