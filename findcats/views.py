3ffcb18a39a8d314e420d794946649d15a487de7
Minhua Xu <minhuaxu@Minhuas-MacBook-Pro.local>
2014-11-08 16:33:01 -0800

    add template

--- findcats/views.py
+++ findcats/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 import urllib2
 
 # Create your views here.
-def index(req):
+def index(request):
     req = urllib2.Request('https://api.imgur.com/3/image/igmyoeU')
     req.add_header('Authorization', 'Client-ID 195fbce43dd974e')
     resp = urllib2.urlopen(req)
     content = resp.read()
-    return HttpResponse(content)
+
+    context = {'latest_poll_list': content}
+    return render(request, 'findcats/index.html', context)
+    #return HttpResponse(content)
 
 def results(request, cat_id):
     return HttpResponse("You're looking at the results of poll %s." % cat_id)
