28666ae01e8f89f5b4da6d652633248d76acfdfe
sureleo <surezeroleo@gmail.com>
2014-11-08 19:35:09 -0800

    add js and css

--- findcats/urls.py
+++ findcats/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from findcats import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<cat_id>\d+)/results/$', views.results, name='results'),
-)
+    url(r'^(?P<cat_id>\d+)/results/$', views.results, name='results')
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
